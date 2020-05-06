63c7b1c3b97615edbeaddce29a7c36898ce0a835
Nam Nguyen <nguyen.1996@osu.edu>
2020-05-06 16:56:54 -0400

    Update pratice_problem.py
    Fix bugs

--- CPTS215WSU/Lessons/U1 Python for Data Analysis/B. Python-Basics-II/pratice_problem.py
+++ CPTS215WSU/Lessons/U1 Python for Data Analysis/B. Python-Basics-II/pratice_problem.py
@@ -3,7 +3,7 @@ def open_input_file():
 	Read the words.txt in current directory
 	Return the file object associated with words.txt
 	'''
-	return open(r"words.txt",r)
+	return open("words.txt","r")
 
 def close_file(file):
 	'''
@@ -12,19 +12,23 @@ def close_file(file):
 	'''
 	file.close()
 
-def first_five_words():
+def first_five_words(file):
 	'''
+	take file as arguments
 	displays first 5 words of the file
 	each words on each line
+	no extra line after output
 	'''
 	for i in range(5):
 		line = file.readline()
-		print("%s" %(line), end = "\n")
+		print("%s" %(line), end = "")
 
 def main():
 	'''
 	main output of the program
 	'''	
 	file = open_input_file()
-	first_five_words()
+	first_five_words(file)
 	close_file(file)
+
+main()
